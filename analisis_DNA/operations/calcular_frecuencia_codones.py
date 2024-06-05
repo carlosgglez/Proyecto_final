4aeb55f68474d4d39339a0813bd5964651c6792a
carlosgglez <carlosgg@lcg.unam.mx>
2024-06-04 22:39:36 -0600

    Se documento el archivo calcular frecuencia de codones

--- analisis_DNA/operations/calcular_frecuencia_codones.py
+++ analisis_DNA/operations/calcular_frecuencia_codones.py
@@ -1,5 +1,30 @@
-def calculate_codon_frequency(sequence):
+'''
+codon-frequency.py: Modulo para calcular el contenido de cada codon en secuencias de DNA.
 
+Este módulo proporciona funciones para determinar el porcentaje de aparicipon de cada codon 
+en una secuencia de ADN dada. Esto es util para estudios de ingeneria genetica ya que optimizar
+la expresión de genes en sistemas heterólogos. Al seleccionar codones preferidos por el organismo 
+huésped, se puede aumentar la eficiencia de la traducción y la producción de proteínas recombinantes.
+
+Funciones:
+- calculate_codon_frequency(sequence): Devuelve el porcentaje de apariciones de cada codon en la secuencia.
+
+'''
+
+def calculate_codon_frequency(sequence):
+    '''
+    Calcula el contenido porcentual de cada codon en una secuencia de ADN.
+
+    Args:
+        sequence (str): La secuencia de ADN a analizar.
+
+    Returns:
+        float: El porcentaje de contenido de cada codon en la secuencia.
+
+    Raises:
+        ValueError: Si la secuencia está vacía o contiene caracteres no válidos.
+    '''
+    #Validación básica de la secuencia
     if not sequence:
         raise ValueError("La secuencia proporcionada está vacía.")
     
