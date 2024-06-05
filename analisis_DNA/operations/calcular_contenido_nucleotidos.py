13fc16d5bbcb29b6be267fb34ef09700c92295cb
carlosgglez <carlosgg@lcg.unam.mx>
2024-06-05 00:12:31 -0600

    Se documento el  archivo para calcular el contenido de cada nucleotido

--- analisis_DNA/operations/calcular_contenido_nucleotidos.py
+++ analisis_DNA/operations/calcular_contenido_nucleotidos.py
@@ -1,4 +1,25 @@
+'''
+calcular_contenido_nucleotidos: Modulo para calcular el contenido de cada nucleotido en secuencias de DNA.
+
+Este módulo proporciona funciones para determinar las veces que aparece un nucleotido  
+en una secuencia de ADN dada. Esto es util para estudios genéticos y genómicos ya que proporciona
+informacion importante para la comprensión y manipulación del material genético.
+
+Funciones:
+- calculate_nucleotide_content(DNA, nucleotide): Devuelve el número de apariciones los nucleotidos en la secuencia.
+'''
+
 def calculate_nucleotide_content(DNA, nucleotide):
+    '''
+    Calcula el contenido porcentual de cada codon en una secuencia de ADN.
+
+    Args:
+        DNA (str): La secuencia de ADN a analizar.
+        nucleotide (str): El nucleotido o nucleotidos a contar.
+
+    Returns:
+        int: El número de veces que aparece un nucleótido específico o los 4 de una secuencia dada.
+    '''
     
     if nucleotide=="A":
         print(f"El total de Adeninas es:{DNA.upper().count('A')}") 
